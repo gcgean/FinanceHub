a622d7519fed408a762ebeaae8d79cb9c290a043
gcgean <81776909+gcgean@users.noreply.github.com>
2026-04-28 12:56:06 -0300

    allow vite app host

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,14 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "localhost",
-    port: 5173,
-    hmr: {
-      overlay: true,
-    },
+ server: {
+  host: true,
+  port: 5173,
+  allowedHosts: "all",
+  hmr: {
+    overlay: true,
   },
+},
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
